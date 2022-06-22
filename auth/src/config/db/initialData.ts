5cb07644c99e0aa6d1dee2923d72aa2925cb7c24
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-06-22 20:26:26 -0300

    adding sequelize configuration

---
+++ auth/src/config/db/initialData.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+import User from "modules/user/model/User";
+
+export const createInitialData = async () => {
+  const saltRounds = 10;
+  try {
+    await User.sync({ force: true });
+
+    const encryptedPassword = await bcrypt.hash("qwe123", saltRounds);
+
+    await User.create({
+      name: "TestUser",
+      email: "user@email.com",
+      password: encryptedPassword,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
