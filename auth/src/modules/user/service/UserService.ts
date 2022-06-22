80075d51ab5d3be10fe4c59cf9fd60c49f9e7506
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-06-22 20:27:25 -0300

    creating user service

---
+++ auth/src/modules/user/service/UserService.ts
@@ -0,0 +1,34 @@
+import statusCodes from "config/constants/statusCodes";
+import { Request } from "express";
+import UserRepository from "../repository/UserRepository";
+
+class UserService {
+  constructor(private repository: UserRepository) {}
+
+  public async findByEmail(request: Request) {
+    try {
+      const { email } = request.params;
+
+      if (!email) {
+        throw new Error("Email not provided.");
+      }
+
+      const user = await this.repository.findByEmail(email);
+
+      if (!user) {
+
+      }
+
+      return {}
+    } catch (err) {
+      return {
+        status: (err as any).status
+          ? (err as any).status
+          : statusCodes.INTERNAL_ERROR,
+        message: (err as any).message,
+      };
+    }
+  }
+}
+
+export default new UserService(new UserRepository());
