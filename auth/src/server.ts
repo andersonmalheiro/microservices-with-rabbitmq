e05c6f32656dc49ff6222926c6de3a876fdd528f
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-06-22 20:27:34 -0300

    updating server.ts

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,6 +1,9 @@
+import * as db from "config/db/initialData";
 import app from "./app";
 
 const port = process.env.PORT || 8080;
+
+db.createInitialData();
 
 app.listen(port, () => {
   console.log(`Auth API running on http://localhost:${port} 🚀`);
