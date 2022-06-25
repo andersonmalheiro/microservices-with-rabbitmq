193f1cbfb561261929752e1d6157006650efca12
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-06-25 15:58:17 -0300

    fixing imports

--- auth/src/config/db/initialData.ts
+++ auth/src/config/db/initialData.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import User from "modules/user/model/User";
+import User from "../../modules/user/model/User";
 
 export const createInitialData = async () => {
   const saltRounds = 10;
