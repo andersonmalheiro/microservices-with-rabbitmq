9c3e1d9318ea3e20b6c59aef3a43674bab6f60cf
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-06-27 09:15:18 -0300

    fixing paths and build params

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,9 +1,11 @@
-import * as db from "./config/db/initialData";
+import * as db from "@config/db/initialData";
 import app from "./app";
 
 const port = process.env.PORT || 8080;
 
-db.createInitialData();
+if (process.env.NODE_ENV === 'development') {
+  db.createInitialData();
+}
 
 app.listen(port, () => {
   console.log(`Auth API running on http://localhost:${port} 🚀`);
