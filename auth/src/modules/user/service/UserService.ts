24fe4c569f9862fe5fdcf60677ce839a6c3362a1
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-09-08 17:36:31 -0300

    improving error handling on service

--- auth/src/modules/user/service/UserService.ts
+++ auth/src/modules/user/service/UserService.ts
@@ -1,10 +1,13 @@
 import statusCodes from "@config/constants/statusCodes";
 import { Request, Response } from "express";
-import { isRight, unwrapEither } from "utils/functions/either";
-import { ErrorResponse, CustomResponse } from "utils/models/Request";
+import { isLeft, unwrapEither } from "utils/functions/either";
+import { InternalError, NotFoundError } from "utils/models/Error";
+import { CustomResponse } from "utils/models/Request";
 import { emailValidator } from "utils/validators";
 import { UserData } from "../model/User";
 import UserRepository from "../repository/UserRepository";
+
+type UserErrorResponse = Response<NotFoundError> | Response<InternalError>;
 
 class UserService {
   constructor(private repository: UserRepository) {}
@@ -12,32 +15,39 @@ class UserService {
   public findByEmail = async (
     request: Request,
     response: Response
-  ): Promise<ErrorResponse | CustomResponse<UserData>> => {
-    const { email } = request.query;
+  ): Promise<UserErrorResponse | CustomResponse<UserData>> => {
+    try {
+      const { email } = request.query;
 
-    const eitherEmailOrError = emailValidator(email as string);
+      const emailOrError = emailValidator(email as string);
 
-    if (isRight(eitherEmailOrError)) {
-      const eitherUserOrNull = await this.repository.findByEmail(
-        email as string
-      );
+      if (isLeft(emailOrError)) {
+        const emailError = unwrapEither(emailOrError);
 
-      if (isRight(eitherUserOrNull)) {
-        const user = unwrapEither(eitherUserOrNull);
-        return response.json(user).send();
+        return response
+          .status(statusCodes.BAD_REQUEST)
+          .json({ message: emailError.message })
+          .send();
       }
 
+      const userOrError = await this.repository.findByEmail(email as string);
+
+      if (isLeft(userOrError)) {
+        const userError = unwrapEither(userOrError);
+        return response
+          .status(userError.statusCode)
+          .json({ message: userError.message })
+          .send();
+      }
+
+      const user = unwrapEither(userOrError);
+      return response.json(user).send();
+    } catch (error) {
       return response
-        .status(statusCodes.NOT_FOUND)
-        .json({ message: "User not found" })
+        .status(statusCodes.INTERNAL_ERROR)
+        .json({ message: (error as Error).message })
         .send();
     }
-
-    const error = unwrapEither(eitherEmailOrError);
-    return response
-      .status(statusCodes.BAD_REQUEST)
-      .json({ message: error.message })
-      .send();
   };
 }
 
