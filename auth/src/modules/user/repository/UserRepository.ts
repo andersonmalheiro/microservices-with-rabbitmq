6a405c56e025463c344db9603904bd3dceebcdad
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-09-08 14:12:57 -0300

    refactoring repository and service to use Either types

--- auth/src/modules/user/repository/UserRepository.ts
+++ auth/src/modules/user/repository/UserRepository.ts
@@ -1,9 +1,11 @@
-import User from "../model/User";
+import { makeLeft, makeRight } from "utils/functions/either";
+import { Either } from "utils/models/Either";
+import UserModel, { UserData } from "../model/User";
 
 export default class UserRepository {
   async findById(id: number) {
     try {
-      return await User.findOne({
+      return await UserModel.findOne({
         where: { id },
       });
     } catch (error) {
@@ -11,13 +13,19 @@ export default class UserRepository {
     }
   }
 
-  public async findByEmail(email: string) {
-    try {
-      return await User.findOne({
-        where: { email },
-      });
-    } catch (error) {
-      console.error(error);
+  public async findByEmail(
+    userEmail: string
+  ): Promise<Either<null | undefined, UserData>> {
+    const result = await UserModel.findOne({
+      where: { email: userEmail },
+    });
+
+    if (!result) {
+      return makeLeft(null);
     }
+
+    const { id, name, email, createdAt, updatedAt, deletedAt } = result;
+
+    return makeRight({ id, name, email, createdAt, updatedAt, deletedAt });
   }
 }
--- auth/src/modules/user/service/UserService.ts
+++ auth/src/modules/user/service/UserService.ts
@@ -1,34 +1,44 @@
 import statusCodes from "@config/constants/statusCodes";
-import { Request } from "express";
+import { Request, Response } from "express";
+import { isRight, unwrapEither } from "utils/functions/either";
+import { ErrorResponse, CustomResponse } from "utils/models/Request";
+import { emailValidator } from "utils/validators";
+import { UserData } from "../model/User";
 import UserRepository from "../repository/UserRepository";
 
 class UserService {
   constructor(private repository: UserRepository) {}
 
-  public async findByEmail(request: Request) {
-    try {
-      const { email } = request.params;
+  public findByEmail = async (
+    request: Request,
+    response: Response
+  ): Promise<ErrorResponse | CustomResponse<UserData>> => {
+    const { email } = request.query;
 
-      if (!email) {
-        throw new Error("Email not provided.");
+    const eitherEmailOrError = emailValidator(email as string);
+
+    if (isRight(eitherEmailOrError)) {
+      const eitherUserOrNull = await this.repository.findByEmail(
+        email as string
+      );
+
+      if (isRight(eitherUserOrNull)) {
+        const user = unwrapEither(eitherUserOrNull);
+        return response.json(user).send();
       }
 
-      const user = await this.repository.findByEmail(email);
-
-      if (!user) {
-
-      }
-
-      return {}
-    } catch (err) {
-      return {
-        status: (err as any).status
-          ? (err as any).status
-          : statusCodes.INTERNAL_ERROR,
-        message: (err as any).message,
-      };
+      return response
+        .status(statusCodes.NOT_FOUND)
+        .json({ message: "User not found" })
+        .send();
     }
-  }
+
+    const error = unwrapEither(eitherEmailOrError);
+    return response
+      .status(statusCodes.BAD_REQUEST)
+      .json({ message: error.message })
+      .send();
+  };
 }
 
 export default new UserService(new UserRepository());
