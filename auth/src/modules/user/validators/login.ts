18b031cf47ec70e32de6d040631aaf69acc29cee
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-09-28 15:20:56 -0300

    [auth] - adding login validators

---
+++ auth/src/modules/user/validators/login.ts
@@ -0,0 +1,43 @@
+import {
+  isLeft,
+  makeLeft,
+  makeRight,
+  unwrapEither,
+} from "@utils/functions/either";
+import { Either } from "@utils/models/Either";
+import { BadRequestError, ForbiddenError } from "@utils/models/Error";
+import { emailValidator, passwordValidator } from "@utils/validators";
+import Login from "../model/Login";
+import bcrypt from "bcrypt";
+
+export const loginValidator = (data: Login): Either<BadRequestError, true> => {
+  const { email, password } = data;
+
+  const emailOrError = emailValidator(email);
+  const passwordOrError = passwordValidator(password);
+
+  if (isLeft(emailOrError)) {
+    const emailError = unwrapEither(emailOrError);
+    return makeLeft(new BadRequestError(emailError.message));
+  }
+
+  if (isLeft(passwordOrError)) {
+    const passwordError = unwrapEither(passwordOrError);
+    return makeLeft(new BadRequestError(passwordError.message));
+  }
+
+  return makeRight(true);
+};
+
+export const userPasswordValidator = async (
+  password: string,
+  hash: string
+): Promise<Either<ForbiddenError, true>> => {
+  const isValid = await bcrypt.compare(password, hash);
+
+  if (!isValid) {
+    return makeLeft(new ForbiddenError("Incorrect password"));
+  }
+
+  return makeRight(true);
+};
