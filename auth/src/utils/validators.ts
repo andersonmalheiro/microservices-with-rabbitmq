381c86cefc596b9d08d7ffd69d547b6ae9fbc2de
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-09-28 15:22:33 -0300

    [auth] - adding password validator on validators

--- auth/src/utils/validators.ts
+++ auth/src/utils/validators.ts
@@ -1,7 +1,7 @@
 import { makeLeft, makeRight } from "./functions/either";
 import { Either } from "./models/Either";
 
-export const emailValidator = (value?: string): Either<Error, boolean> => {
+export const emailValidator = (value?: string): Either<Error, true> => {
   if (!value) {
     return makeLeft(new Error("Provided email must not be null or empty"));
   }
@@ -12,6 +12,27 @@ export const emailValidator = (value?: string): Either<Error, boolean> => {
 
   if (!valueIsValid || typeof value !== "string") {
     return makeLeft(new Error("Provided email is not valid"));
+  }
+
+  return makeRight(true);
+};
+
+export const passwordValidator = (value?: string): Either<Error, true> => {
+  const MIN_LENGTH = 6;
+  const SPACE = " ";
+
+  if (!value) {
+    return makeLeft(new Error("Password must not be null or empty"));
+  }
+
+  if (value.includes(SPACE)) {
+    return makeLeft(new Error("Password must not contain spaces"));
+  }
+
+  if (value.length < MIN_LENGTH) {
+    return makeLeft(
+      new Error(`Password must have at least ${MIN_LENGTH} characters`)
+    );
   }
 
   return makeRight(true);
