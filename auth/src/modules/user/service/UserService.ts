03a37c54136ec4c8cd42ed117d6e392f4e6d4c82
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-09-28 15:23:07 -0300

    [auth] - adding getAccessToken method on UserService class

--- auth/src/modules/user/service/UserService.ts
+++ auth/src/modules/user/service/UserService.ts
@@ -3,11 +3,14 @@ import { Request, Response } from "express";
 import { isLeft, unwrapEither } from "utils/functions/either";
 import { InternalError, NotFoundError } from "utils/models/Error";
 import { CustomResponse } from "utils/models/Request";
-import { emailValidator } from "utils/validators";
-import { UserData } from "../model/User";
+import { emailValidator } from "@utils/validators";
+import { loginValidator, userPasswordValidator } from "../validators/login";
+import { User } from "../model/User";
 import UserRepository from "../repository/UserRepository";
+import jwt from "jsonwebtoken";
+import { apiSecret } from "@config/constants/secrets";
 
-type UserErrorResponse = Response<NotFoundError> | Response<InternalError>;
+type ErrorResponse = Response<NotFoundError> | Response<InternalError>;
 
 class UserService {
   constructor(private repository: UserRepository) {}
@@ -15,7 +18,7 @@ class UserService {
   public findByEmail = async (
     request: Request,
     response: Response
-  ): Promise<UserErrorResponse | CustomResponse<UserData>> => {
+  ): Promise<ErrorResponse | CustomResponse<Omit<User, "password">>> => {
     try {
       const { email } = request.query;
 
@@ -40,7 +43,8 @@ class UserService {
           .send();
       }
 
-      const user = unwrapEither(userOrError);
+      const { password, ...user } = unwrapEither(userOrError);
+
       return response.json(user).send();
     } catch (error) {
       return response
@@ -49,6 +53,63 @@ class UserService {
         .send();
     }
   };
+
+  public async getAccessToken(
+    request: Request,
+    response: Response
+  ): Promise<ErrorResponse | undefined> {
+    try {
+      const { email, password } = request.body;
+
+      const loginOrError = loginValidator({ email, password });
+
+      if (isLeft(loginOrError)) {
+        const loginError = unwrapEither(loginOrError);
+        return response
+          .status(loginError.statusCode)
+          .json({ message: loginError.message })
+          .send();
+      }
+
+      const userOrError = await this.repository.findByEmail(email as string);
+
+      if (isLeft(userOrError)) {
+        const userError = unwrapEither(userOrError);
+        return response
+          .status(userError.statusCode)
+          .json({ message: userError.message })
+          .send();
+      }
+
+      const user = unwrapEither(userOrError);
+
+      const passwordOrError = await userPasswordValidator(
+        password,
+        user.password
+      );
+
+      if (isLeft(passwordOrError)) {
+        const passwordError = unwrapEither(passwordOrError);
+        return response
+          .status(passwordError.statusCode)
+          .json({ message: passwordError.message })
+          .send();
+      }
+
+      const authUser = { id: user.id, name: user.name, email };
+      const accessToken = jwt.sign(authUser, apiSecret, { expiresIn: "1d" });
+
+      return response
+        .status(statusCodes.SUCCESS)
+        .json({ accessToken })
+        .send();
+    } catch (error) {
+      return response
+        .status(statusCodes.INTERNAL_ERROR)
+        .json({ message: (error as Error).message })
+        .send();
+    }
+  }
 }
 
 export default new UserService(new UserRepository());
