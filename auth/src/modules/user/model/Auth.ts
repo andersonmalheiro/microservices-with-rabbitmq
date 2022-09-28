35f3bb0d2c283b01da2066bf6255e47ca2849f7f
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-09-28 17:23:36 -0300

    [auth] - renaming Login model to Auth

---
+++ auth/src/modules/user/model/Auth.ts
@@ -0,0 +1,10 @@
+export interface Login {
+  email: string;
+  password: string;
+}
+
+export interface AuthUser {
+  id: number;
+  name: string;
+  email: string;
+}
--- auth/src/modules/user/model/Login.ts
+++
@@ -1,6 +0,0 @@
-interface Login {
-    email: string;
-    password: string;
-}
-
-export default Login;
